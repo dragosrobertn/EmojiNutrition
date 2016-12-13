65965fb643a37dddd44ffccba5563315779ba1c5
dragosrobertn <hello@dragosneagu.com>
2016-12-13 00:20:43 +0000

    Write function to build emojis

--- app/src/main/java/com/dragosneagu/emojinutrition/Emoji.java
+++ app/src/main/java/com/dragosneagu/emojinutrition/Emoji.java
@@ -6,7 +6,22 @@ package com.dragosneagu.emojinutrition;
 
 public class Emoji {
 
-    public String getEmojiByUnicode(int unicode){
-        return new String(Character.toChars(unicode));
+    public String getEmojiCombinationByUnicode(int unicode1, int unicode2, int unicode3, int unicode4) {
+        String full = new String();
+
+        if (!(unicode1 == 0))
+            full += new String(Character.toChars(unicode1));
+
+        if (!(unicode2 == 0))
+            full += new String(Character.toChars(unicode2));
+
+        if (!(unicode3 == 0))
+            full += new String(Character.toChars(unicode3));
+
+        if (!(unicode4 == 0))
+            full += new String(Character.toChars(unicode4));
+
+        return full;
+
     }
 }
--- app/src/main/java/com/dragosneagu/emojinutrition/StartActivity.java
+++ app/src/main/java/com/dragosneagu/emojinutrition/StartActivity.java
@@ -3,11 +3,14 @@ package com.dragosneagu.emojinutrition;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
     TextView introTextView;
+    Button avatarChoiceM, avatarChoiceF;
+
     Emoji emoji = new Emoji();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +19,14 @@ public class StartActivity extends AppCompatActivity {
         // We set this as a string.
         setTitle(getString(R.string.start_title));
         introTextView = (TextView) findViewById(R.id.introTextView);
-        introTextView.setText(String.format(getString(R.string.intro_text_view), emoji.getEmojiByUnicode((int)0x1F60A)));
+        avatarChoiceF = (Button) findViewById(R.id.avatarChoiceF);
+        avatarChoiceM = (Button) findViewById(R.id.avatarChoiceM);
+        // Time to play with emojis!!!
+        introTextView.setText(String.format(getString(R.string.intro_text_view), emoji.getEmojiCombinationByUnicode(0x1F60A, 0, 0, 0)));
+        avatarChoiceF.setTextSize(120f);
+        avatarChoiceM.setTextSize(120f);
+        avatarChoiceF.setText(emoji.getEmojiCombinationByUnicode(0x1F64B, 0, 0, 0));
+        avatarChoiceM.setText(emoji.getEmojiCombinationByUnicode(0x1F64B, 0x200D, 0x2642, 0xFE0F));
     }
 
 }
